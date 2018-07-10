01d2b1c16023c0cb91ec3e6f96e5fee8a55999fb
Jim Beck <jamesbeckjr@gmail.com>
2018-07-10 10:21:59 -0400

    Created RollButton Function
    Pressing the Roll button generates a number between 1 and 6.
    Roll Button displays the number next to it.

---
+++ Board-Game-Racing/Assets/Scripts/RollButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class RollButton : MonoBehaviour
+{
+    public int numberOfSpacesToMove { get; private set; }
+
+    private void GenerateRandomNumber()
+    {
+        numberOfSpacesToMove = Random.Range(1, 7);
+    }
+
+    public void DisplayNumberOfSpacesToMove()
+    {
+        GenerateRandomNumber();
+        TextMeshProUGUI rollButtonDisplay = GetComponentInChildren<TextMeshProUGUI>();
+        rollButtonDisplay.text = numberOfSpacesToMove.ToString();
+    }
+}
